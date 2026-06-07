de15c5a56e89c2d3017591d50879a96e9ffe0aa9
Abromo79 <saidhamad3437@gmail.com>
2026-06-07 12:04:52 +0300

    route update

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    
+        return redirect()->route('admin.dashboard');
+   
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
